a1c2dca8efb9af2f3977bf9bb05e82ac62cf31fc
Unish Rajkarnikar <40193412+ubidy947@users.noreply.github.com>
2021-03-18 14:11:45 +0545

    dateFormatting and dart version upgrade

--- lib/transaction.dart
+++ lib/transaction.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 
 class Transaction {
   final String id;
@@ -7,8 +6,8 @@ class Transaction {
   final DateTime date;
 
   Transaction({
-    @required this.id, 
-    @required this.title, 
-    @required this.amount, 
-    @required this.date});
+    required this.id, 
+    required this.title, 
+    required this.amount, 
+    required this.date});
 }
