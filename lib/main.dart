28b3ba4114f8be407ff7c4e167f59a4c571d1ca4
Unish Rajkarnikar <40193412+ubidy947@users.noreply.github.com>
2021-03-23 08:53:31 +0545

    understanding widgetllifecycle and app lifecycle

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import './widgets/new_transaction.dart';
 // import 'package:flutter/services.dart';
 import './widgets/transaction_list.dart';
@@ -56,7 +57,7 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   final List<Transaction> _userTransactions = [
     // Transaction(
     //   id: 't1',
@@ -73,6 +74,23 @@ class _MyHomePageState extends State<MyHomePage> {
   ];
 
   bool _showChart = false;
+
+  @override
+    void initState() {
+      WidgetsBinding.instance.addObserver(this);
+      super.initState();
+    }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print(state);
+  }
+
+  @override
+  dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
 
   List<Transaction> get _recentTransactions {
     return _userTransactions.where((tx) {
